c7482b2b55e51a22cc4a8e215c5a899428f95a13
unknown <install@DIPLOMADO128.computo.acatlan.unam.mx>
2018-04-21 13:46:38 -0500

    JS

--- clase_8/code1_2.php
+++ clase_8/code1_2.php
@@ -1,7 +1,7 @@
 <?php
 $fecha=new DateTime;
 echo $fecha->format("d/m/Y H:i:s")."<br/>";
-$fecha_2=new DateTime("2038-01-19");
+$fecha_2=new DateTime("2138-01-19");
 echo $fecha_2->format("d/m/Y H:i:s")."<br/>";
 $fecha_3=new DateTime("now");
 echo $fecha_3->format("d/m/Y H:i:s")."<br/>";
@@ -10,8 +10,8 @@ echo $fecha_3->format("d/m/Y H:i:s")."<br/>";
 $fecha_3->setTimeZone(new DateTimeZone("America/Mexico_City"));
 echo $fecha_3->format("d/m/Y H:i:s")."<br/>";
 echo $fecha->format("d/m/Y H:i:s")."<br/>";
-$fechaDiff=$fecha_3->diff($fecha);
+$fechaDiff=$fecha_2->diff($fecha);
 echo $fechaDiff->format('%R%a days')."<br/>";
-$fechaDiff_2=$fecha->diff($fecha_3);
+$fechaDiff_2=$fecha->diff($fecha_2);
 echo $fechaDiff_2->format('%R%a days')."<br/>";
 ?>
--- js/code1_1.js
+++ js/code1_1.js
@@ -1,7 +1,19 @@
 var button=document.getElementById("example");
 button.addEventListener("click",sendAlert);
 function sendAlert(){
-	alert("HOLA");
+	//alert("HOLA");
+	modifyByClass("perro");
+	modifyByName("color");
+	modifyByTag("span");
+	/*var divpopo=document.getElementById("divpopo");
+	getDataAttr(divpopo,"data-numero");
+	modifyAttribute(divpopo,"data-numero","10000000");
+	getDataAttr(divpopo,"data-numero");
+	removeAttribute(divpopo,"data-numero");
+	getDataAttr(divpopo,"data-numero");
+	removeElement(divpopo);*/
+	var inputNombre=document.getElementById("nombre");
+	getInputValue(inputNombre);
 }
 
 function modifyByClass(classname){
@@ -22,21 +34,24 @@ function modifyByName(name){
 }
 
 function modifyByTag(tag){
-	var x = document.getElementsByTag(tag);
+	//alert(x);
+	var x = document.getElementsByTagName(tag);
+
 	for (i = 0; i < x.length; i++) {
+		//alert("no");
 	    x[i].innerHTML = "modificado";
 	}
 }
 
 function getDataAttr(element,atribute){
-	if(element.hasAttribute())
+	if(element.hasAttribute(atribute))
 	{
 		var attr=element.getAttribute(atribute);
 	}
 	alert(attr);
 }
 
-function updateAttribute(element,atribute){
+function removeAttribute(element,atribute){
 	if(element.hasAttribute(atribute))
 	{
 		element.removeAttribute(atribute);
@@ -56,8 +71,8 @@ function removeElement(element)
 	element.remove();
 }
 
-function getInputValue(inputID){
-	var input=document.getElementById(inputID);
+function getInputValue(input){
+	//var input=document.getElementById(inputID);
 	alert(input.value);
 }
 
@@ -67,7 +82,7 @@ function exampleAjax()
 	xhttp.responseType = "json";
 	xhttp.onreadystatechange = function() {
 	if (this.readyState == 4 && this.status == 200) {
-			document.getElementById("demo").innerHTML = this.response;
+			document.getElementById("demo").innerHTML = this.response.edad;
 			var respuesta=this.response;
 			console.log(respuesta);
 			console.log(respuesta.nombre)
@@ -77,5 +92,5 @@ function exampleAjax()
 
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-	xhttp.send("example=1");
+	xhttp.send("example=1&perro=2");
 }
